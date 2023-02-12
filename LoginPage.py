513e1838fab8746dd1306c4fefbcc7120a0e4619
Shashank Gupta <117008526+ShashankGupta10@users.noreply.github.com>
2023-02-12 12:14:26 +0530

    Update LoginPage.py

--- LoginPage.py
+++ LoginPage.py
@@ -5,7 +5,7 @@ from PIL import ImageTk, Image
 class LoginPage:
     def __init__(self, window):
         self.window = window
-        self.window.geometry('1166x718')
+        self.window.geometry('1920x1080')
         self.window.resizable(0, 0)
         self.window.state('zoomed')
         self.window.title('Login Page')
@@ -13,14 +13,15 @@ class LoginPage:
         # ========================================================================
         # ============================background image============================
         # ========================================================================
-        self.bg_frame = Image.open("D:\Pyfon MPR\TkinterGUI\images\Background1.png")
-        photo = ImageTk.PhotoImage(self.bg_frame)
+        self.bg_frame = Image.open("D:\Pyfon MPR\TkinterGUI\images\Background.jpg")
+        a = self.bg_frame.resize((1920,1080))
+        photo = ImageTk.PhotoImage(a)
         self.bg_panel = Label(self.window, image=photo)
         self.bg_panel.image = photo
         self.bg_panel.pack(fill='both', expand='yes')
         # ====== Login Frame =========================
         self.lgn_frame = Frame(self.window, bg='#040405', width=950, height=600)
-        self.lgn_frame.place(x=200, y=70)
+        self.lgn_frame.place(x=300, y=115)
 
         # ========================================================================
         # ========================================================
