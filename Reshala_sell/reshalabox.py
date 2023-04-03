b42d7d6d9ee4fdbe91e4dd741f0da398f5ed411d
Shashank Gupta <117008526+ShashankGupta10@users.noreply.github.com>
2023-04-03 21:51:46 +0530

    SHASHANK

--- Reshala_sell/reshalabox.py
+++ Reshala_sell/reshalabox.py
@@ -25,7 +25,89 @@ class HomePage(QMainWindow):
         super().__init__()
         
         self.setWindowTitle("Home Page")
-        self.setGeometry(0,0,1366,768)
+        self.setGeometry(0,0,1920,1080)
+
+
+        self.header = QLabel(self)
+        self.header.setGeometry(0, 0, 1920, 100)
+        self.header.setStyleSheet("QLabel{ background: black; position: fixed;} ")
+
+        logo = QLabel(self)
+        logo.setGeometry(30, 20, 80, 70)
+        self.pixmap = QPixmap("D:\python mpr final\Python-MPR-\loginpage\smalllogo.png")
+        logo.setPixmap(self.pixmap)
+        logo.setScaledContents(True)
+        self.pixmap = self.pixmap.scaled(100, 200)
+
+        navbarbtn1 = QPushButton("Home", self)
+        navbarbtn1.setGeometry(1200, 31, 100, 40)
+        navbarbtn1.setStyleSheet("QPushButton{ background: Black; position: fixed;border-radius:15px;color: white;}")
+        navbarbtn1.setFont(QFont('Times', 20))
+
+
+        navbarbtn2= QPushButton("Reshala", self)
+        navbarbtn2.setGeometry(1400, 31, 150, 40)
+        navbarbtn2.setStyleSheet("QPushButton{ background: Black; position: fixed;border-radius:15px;color: white;}")
+        navbarbtn2.setFont(QFont('Times', 20))
+        
+        navbarbtn3= QPushButton("About", self)
+        navbarbtn3.setGeometry(1600, 31, 150, 40)
+        navbarbtn3.setStyleSheet("QPushButton{ background: Black; position: fixed;border-radius:15px;color: white;}")
+        navbarbtn3.setFont(QFont('Times', 20))
+
+
+        icon = QIcon("D:\Pyfon MPR\TkinterGUI\images\homepageimage1bgrm.png")
+        self.btn10 = QPushButton("" ,self)
+        self.btn10.setGeometry(1800, 0, 100, 100)
+        self.btn10.setStyleSheet("background : black;")
+        self.btn10.setIcon(icon)
+        size = QSize(100, 100)
+        self.btn10.setIconSize(size)
+
+        sidebar = QLabel(self)
+        sidebar.setGeometry(0,100,120,1920)
+        sidebar.setStyleSheet("background-color: #3E54AC;")
+        
+        size = QSize(60, 60)
+        
+        anicon = QIcon('Python-MPR-\\images\\announcements.png.jpg')
+        announce = QPushButton(sidebar)
+        announce.setGeometry(20,30, 60, 60)
+        announce.setStyleSheet("border : 0px solid black")
+        announce.setIcon(anicon)
+        announce.setIconSize(size)
+        
+        
+        aticon = QIcon('D:\\python mpr final\\Python-MPR-\\images\\attendance.png')
+        attend = QPushButton(sidebar)
+        attend.setGeometry(20,150, 60, 60)
+        attend.setStyleSheet("border : 0px solid black")
+        attend.setIcon(aticon)
+        attend.setIconSize(size)
+
+        asicon = QIcon('D:\\python mpr final\\Python-MPR-\\images\\assignment.png.jpg')
+        assign = QPushButton(sidebar)
+        assign.setGeometry(25, 270, 60, 60)
+        assign.setStyleSheet("border : 0px solid black")
+        assign.setIcon(asicon)
+        assign.setIconSize(size)
+
+        reicon = QIcon('D:\\python mpr final\\Python-MPR-\\images\\reshala.png')
+        reshaala = QPushButton(sidebar)
+        reshaala.setGeometry(20,390, 60, 60)
+        reshaala.setStyleSheet("border : 0px solid black")
+        reshaala.setIcon(reicon)
+        reshaala.setIconSize(size)
+        
+        proficon = QIcon('D:\python mpr final\Python-MPR-\images\profile.png-removebg-preview.png')
+        profile = QPushButton(sidebar)
+        profile.setGeometry(20, 700, 60, 60)
+        profile.setStyleSheet("border : 0px solid black")
+        profile.setIcon(proficon)
+        profile.setIconSize(size)
+
+
+
         self.title = QLabel(self)
         self.title.setGeometry(300, 50, 200, 40)
         self.pixmap = QPixmap('images\RESHALA-removebg-preview.png')
@@ -39,32 +121,32 @@ class HomePage(QMainWindow):
 
 
         self.panel1 = QLabel(self)
-        self.panel1.setGeometry(100,140,700,450)
+        self.panel1.setGeometry(610,200,800,600)
         self.panel1.setStyleSheet("QLabel{background: white;  border-radius: 20px; padding: 10px;}")
   
-        self.title = QTextEdit(self.panel1)
-        self.title.setGeometry(150,10,220,50)
-        self.title.setStyleSheet("QTextEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
+        self.title = QLineEdit(self.panel1)
+        self.title.setGeometry(200,50,220,50)
+        self.title.setStyleSheet("QLineEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
 
-        self.price = QTextEdit(self.panel1)
-        self.price.setGeometry(150,70,220,50)
-        self.price.setStyleSheet("QTextEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
+        self.price = QLineEdit(self.panel1)
+        self.price.setGeometry(200,125,220,50)
+        self.price.setStyleSheet("QLineEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
 
-        self.description = QTextEdit(self.panel1)
-        self.description.setGeometry(150,130,220,70)
-        self.description.setStyleSheet("QTextEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
+        self.description = QLineEdit(self.panel1)
+        self.description.setGeometry(200,200,220,70)
+        self.description.setStyleSheet("QLineEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
 
-        self.mail = QTextEdit(self.panel1)
-        self.mail.setGeometry(150,210,220,50)
-        self.mail.setStyleSheet("QTextEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
+        self.mail = QLineEdit(self.panel1)
+        self.mail.setGeometry(200,325,220,50)
+        self.mail.setStyleSheet("QLineEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
 
-        self.number = QTextEdit(self.panel1)
-        self.number.setGeometry(150,270,220,50)
-        self.number.setStyleSheet("QTextEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
+        self.number = QLineEdit(self.panel1)
+        self.number.setGeometry(200,400,220,50)
+        self.number.setStyleSheet("QLineEdit{ background: #EDE3FF; border-color: black; border-radius: 5px; padding: 10px;}")
 
 
-        self.btn6 = QPushButton("Upload" ,self)
-        self.btn6.setGeometry(300, 500, 100, 50)
+        self.btn6 = QPushButton("Upload" ,self.panel1)
+        self.btn6.setGeometry(350, 500, 100, 50)
         self.btn6.setStyleSheet("QPushButton{ background: #580599; color: white; border-radius: 20px; padding: 10px;}"
                                 "QPushButton:hover{ background: #A084DC;border-radius: 10px;}")
         self.btn6.setFont(QFont('Times', 12))
@@ -83,32 +165,32 @@ class HomePage(QMainWindow):
 
         productnametxt = QLabel(self.panel1)
         productnametxt.setText("Product Name :")
-        productnametxt.setGeometry(10, 2,200,50) 
+        productnametxt.setGeometry(10, 50,200,50) 
         productnametxt.setStyleSheet("background-color: transparent;")
         productnametxt.setFont(QFont('Times', 12))
 
 
         productpricetxt = QLabel(self.panel1)
         productpricetxt.setText("Price :")
-        productpricetxt.setGeometry(10, 60,200,50)  
+        productpricetxt.setGeometry(10, 125,200,50)  
         productpricetxt.setStyleSheet("background-color: transparent;")
         productpricetxt.setFont(QFont('Times', 12))
 
         productDescriptiontxt = QLabel(self.panel1)
         productDescriptiontxt.setText("Description : ")
-        productDescriptiontxt.setGeometry(10, 118,200,50) 
+        productDescriptiontxt.setGeometry(10, 200,200,50) 
         productDescriptiontxt.setStyleSheet("background-color: transparent;")
         productDescriptiontxt.setFont(QFont('Times', 12))
 
         Emailtxt = QLabel(self.panel1)
         Emailtxt .setText("Email :")
-        Emailtxt .setGeometry(10, 200,200,50)  
+        Emailtxt .setGeometry(10, 325,200,50)  
         Emailtxt .setStyleSheet("background-color: transparent;")
         Emailtxt .setFont(QFont('Times', 12))
 
         MobileNumbertxt = QLabel(self.panel1)
         MobileNumbertxt.setText("Phone Number:")
-        MobileNumbertxt.setGeometry(10, 258,200,50)  
+        MobileNumbertxt.setGeometry(10, 400,200,50)  
         MobileNumbertxt.setStyleSheet("background-color: transparent;")
         MobileNumbertxt.setFont(QFont('Times', 12))
 
@@ -120,12 +202,12 @@ class HomePage(QMainWindow):
         self.btn1 = QPushButton("Upload Image",self.panel1)
         self.btn1.clicked.connect(self.select_image)
         # self.btn1.setFixedSize(100, 100)
-        self.btn1.setGeometry(450,300,100,40)
+        self.btn1.setGeometry(550,350,100,40)
         self.btn1.setStyleSheet("QPushButton{ background: #C47AFF; position: fixed;border-radius:15px;color: black;border-radius:20px;} QPushButton:hover borderradius:20px;")
         vbox.addWidget(self.btn1)
 
-        self.label = QLabel("Upload product image",self)
-        self.label.setGeometry(500,200,200,200)
+        self.label = QLabel("Upload product image",self.panel1)
+        self.label.setGeometry(500,100,200,200)
         self.label.setStyleSheet("border:2px solid black;")
         self.label.setAlignment(QtCore.Qt.AlignCenter)
 
@@ -135,6 +217,36 @@ class HomePage(QMainWindow):
         vbox.addWidget(self.label)
         self.setLayout(vbox)
 
+
+        self.footer = QLabel(self)
+        self.footer.setGeometry(0, 900, 1920, 1080)
+        self.footer.setStyleSheet("QLabel{ background: black; position: fixed;} ")
+
+        self.label1 = QPushButton(self.footer)
+        self.label1.setGeometry(100,0,150,100)
+        self.label1.setStyleSheet("color: white; background: black;")
+        self.label1.setText("@Academia 2023")
+        self.label1.setFont(QFont('Times', 10))
+
+        self.label2 = QPushButton(self.footer)
+        self.label2.setGeometry(300,0,150,100)
+        self.label2.setStyleSheet("color: white; background: black;")
+        self.label2.setText("Terms of Use")
+        self.label2.setFont(QFont('Times', 10))
+
+        self.label3 = QPushButton(self.footer)
+        self.label3.setGeometry(500,0,200,100)
+        self.label3.setStyleSheet("color: white; background: black;")
+        self.label3.setText("Privacy Policy")
+        self.label3.setFont(QFont('Times', 10))
+                                
+        self.label4 = QPushButton(self.footer)
+        self.label4.setGeometry(1400,0,500,100)
+        self.label4.setStyleSheet("color: white; background: black;")
+        self.label4.setText("Copyright © 2023 Academia Inc. All rights reserved.")
+        self.label4.setFont(QFont('Times', 10))
+
+        
         self.showMaximized()
         self.show()
 
